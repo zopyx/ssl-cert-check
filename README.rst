512d509135f3cf8ca7d8fd0268533d0b2d585b13
Andreas Jung <info@zopyx.com>
2019-11-05 17:57:31 +0100

    updated

--- README.rst
+++ README.rst
@@ -3,3 +3,24 @@ zopyx.check-ssl-domains
 
 Check a list of host/domain names for their expiration date.
 
+Usage
+-----
+
+- create a file domains.txt containing a number of domain names - one per line like:
+
+   www.example.com 
+   www.example2.com
+
+- install `zopyx.check-ssl-domains` using pip
+
+- run `check-ssl-domains domains.txt`
+
+Requirements
+------------
+
+- Python 3.6 or higher
+
+Author
+------
+Andreas Jung/ZOPYX
+info@zopyx.com
--- setup.py
+++ setup.py
@@ -3,10 +3,14 @@ from setuptools import setup
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
    name='zopyx.check_ssl_domains',
    version='1.0',
-   description='Check SSL domains',
+   long_description=long_description,
+   description='Check expiration date of SSL/TLS certs on web host',
    author='Andreas Jung',
    author_email='info@zopyx.com',
    packages=['zopyx'],  #same as name
